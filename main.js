c97d0c7891dda94ff196403f7a8ec6924df8228f
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-03-28 21:23:32 -0400

    added handler fucntion and query element

---
+++ main.js
@@ -0,0 +1,18 @@
+const click = () => {
+// Named function
+}  
+// Query the element 
+const submitButton = document.querySelector('#submit-button');
+// Add the event listener 
+submitButton.addEventListener('click', click);
+
+
+
+
+
+
+
+
+
+
+
